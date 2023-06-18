c9c87e4f9c4ae23cb91d5eb3d27a15ec6df45555
Debo <dwdeante0726@gmail.com>
2023-06-17 19:44:52 -0700

    This will add hidden function to yes and nno buttons.

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var element = document.getElementById("survey");
+  element.classList.remove("hidden");
+}
+function myFunction2() {
+  var element = document.getElementById("survey");
+  element.classList.add("hidden");
+}
